73aed5f00ede03dd40de9d20b90f3847fdda8fae
Shubham Acharya <shubhamacharya@users.noreply.github.com>
2023-07-02 23:28:43 +0530

    fix: Code to deploy two dependent contracts.

--- backend/migrations/1_initial_migration.js
+++ backend/migrations/1_initial_migration.js
@@ -2,6 +2,7 @@ const RNFT = artifacts.require("RNFT");
 const Escrow1155 = artifacts.require("Escrow1155");
 
 module.exports = async function (deployer) {
-  deployer.deploy(RNFT);
-  //deployer.deploy(Escrow1155);
+  deployer.deploy(RNFT).then(() => {
+    return deployer.deploy(Escrow1155, RNFT.address)
+  });
 };
