45c18f4841431bd255b92ac59292ffbcc3c91a59
Shubham Acharya <shubhamacharya@users.noreply.github.com>
2023-07-04 00:07:24 +0530

    fix: GraphQL query error on connecting to GraphQL browser playground.

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,16 @@
 const express = require("express");
+const { graphqlHTTP } = require('express-graphql')
 require("dotenv").config();
 const connectDB = require("./config/db");
 const cors = require("cors");
 const schema = require("./schema/schema")
-const { graphqlHTTP } = require("express-graphql");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.GQL_PORT || 4000;
 
 const app = express();
 
 app.use(cors());
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    graphiql: true
-}));
-
+app.use('/graphql', graphqlHTTP({ schema: schema, graphiql: true }));
 app.listen(PORT, async () => {
     //Connect to DB
     await connectDB();
