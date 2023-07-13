7fdc7b535067ef79f4297cc1593c1c66e2d687d4
Shubham Acharya <shubhamacharya@users.noreply.github.com>
2023-07-13 22:56:49 +0530

    fix: collectionsOfUser returns nftDetails model members.

--- frontend/src/queries/Query.js
+++ frontend/src/queries/Query.js
@@ -1,2 +1,20 @@
 import { gql } from "@apollo/client";
 
+const GET_USER_COLLECTIONS = gql`
+    query collectionsOfUser($ownerAddress: String!) {
+        collectionsOfUser(ownerAddress: $ownerAddress){
+            id,
+            tokenId,
+            name,
+            tokenImg,
+            tokenURI,
+            price,
+            ownerAddress,
+            txId,
+            blockNo,
+            eventData
+        }
+    }
+`;
+
+export { GET_USER_COLLECTIONS }
