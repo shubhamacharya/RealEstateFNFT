48464742c3c72e3e8db146bf79c42576bf4b5d6b
Shubham Acharya <shubhamacharya@users.noreply.github.com>
2023-07-19 16:44:39 +0530

    style: Conditional rendering of mint button to only adminrole users

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
-import { useLocation } from "react-router-dom";
 import { connect } from "../utils/walletConnect";
 
 function NavBar() {
-  let location = useLocation();
   return (
     <Navbar bg="dark" data-bs-theme="dark" sticky="top">
       <Container>
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/home">Home</Nav.Link>
-          {location?.state?.role === "admin" && (
+          {localStorage.getItem("role") === "admin" && (
             <Nav.Link href="/mint">Mint</Nav.Link>
           )}
           <Nav.Link href="/myCollection">Collection</Nav.Link>
