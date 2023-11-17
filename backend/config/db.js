2622c8e3f7c2c714e118f91fe4ec9c1403803259
Shubham Acharya <shubhamacharya@users.noreply.github.com>
2023-11-17 21:11:38 +0530

    feat: Added docker files and docker-compose files.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const con = await mongoose.connect('mongodb://127.0.0.1:27017/RNFT');
+    console.log(process.env.DATABASE_URL+process.env.DATABASE_NAME);
+    const con = await mongoose.connect(process.env.DATABASE_URL+process.env.DATABASE_NAME);
     console.log(`MongoDB Connected : ${con.connection.host}`);
 }
 
